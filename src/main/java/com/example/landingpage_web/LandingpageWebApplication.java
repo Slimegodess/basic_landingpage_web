230bdc885bd8a17419be006a718d9de86953b5a1
PekingDang <50366496+slimegodess@users.noreply.github.com>
2021-04-27 22:15:44 +0200

    Update README.md

---
+++ src/main/java/com/example/landingpage_web/LandingpageWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.landingpage_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandingpageWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandingpageWebApplication.class, args);
+	}
+
+}
