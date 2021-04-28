fc1f48e8965307e639685da8f7e6987988ffcb9e
Slimegodess <s0554758@htw-berlin.de>
2021-04-28 13:50:58 +0200

    save file

--- src/main/java/com/example/landingpage_web/LandingpageWebApplication.java
+++ src/main/java/com/example/landingpage_web/LandingpageWebApplication.java
@@ -1,13 +1,15 @@
 package com.example.landingpage_web;
 
+import com.example.landingpage_web.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class LandingpageWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LandingpageWebApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/landingpage_web/Student/StudentService.java
@@ -0,0 +1,24 @@
+package com.example.landingpage_web.Student;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Willma",
+                        "Willma.Kuss@vondir.com",
+                        LocalDate.of(2000, Month.JULY, 5),
+                        21
+                )
+        );
+    }
+}
