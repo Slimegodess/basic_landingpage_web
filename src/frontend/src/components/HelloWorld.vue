3b58faffab0053ec42461f4f6be7f0811fa5fe89
Slimegodess <s0554758@htw-berlin.de>
2021-05-05 00:51:05 +0200

    Update README.md

---
+++ src/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    date()
+        return{
+            msg:    ''
+        }
+    }
+    mounted(){
+        fetch(input:"/api/messages/hello")
+            .then((response) => response.text())
+            .then((data) => {
+                this.nsg = data;
+            })
+    }
+}
+</script>
