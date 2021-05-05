5468a2f34ed39eb7d80ab5c62eab1a51a8a901cb
Slimegodess <s0554758@htw-berlin.de>
2021-05-05 11:20:35 +0200

    Update README.md

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     // https://cli.vuejs.org/config/#devserver-proxy
     devServer: {
-        port: 8080,
+        port: 3000,
         proxy: {
             '/api': {
                 target: 'http://localhost:8080',
