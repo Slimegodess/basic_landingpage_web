ac99d31d04cdd8b4994c043e7b62f40d6b3cdb51
Slimegodess <s0554758@htw-berlin.de>
2021-05-26 17:25:19 +0200

    GUI on thymeleaf studentstable test example

--- src/main/java/com/example/landingpage_web/Student/StudentController.java
+++ src/main/java/com/example/landingpage_web/Student/StudentController.java
@@ -1,13 +1,13 @@
 package com.example.landingpage_web.Student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "api")
+@Controller
 public class StudentController {
 
     private final StudentService studentService;
@@ -15,16 +15,6 @@ public class StudentController {
     @Autowired
     public StudentController(StudentService studentService){
         this.studentService = studentService;
-    }
-
-    @GetMapping
-    public List<Student> getStudents(){
-        return studentService.getStudents();
-    }
-
-    @PostMapping
-    public void registerNewStudent(@RequestBody Student student){
-        studentService.addNewStudent(student);
     }
 
     @DeleteMapping(path = "{studentId}")
@@ -40,18 +30,23 @@ public class StudentController {
         studentService.updateStudent(studentId, name, email);
     }
 
-    @PostMapping("/addstudent")
-    public String studentSubmit(@ModelAttribute Student student, Model model){
+    @PostMapping(path ="/addstudent")
+    public String addNewStudent(@ModelAttribute Student student, Model model){
         studentService.addNewStudent(student);
         model.addAttribute("student",student);
         return "studentresult";
     }
 
-    @GetMapping("/liststudents")
+    @GetMapping(path ="/addstudent")
+    public String addNewStudent(Model model){
+        model.addAttribute("student", new Student());
+        return "addstudent";
+    }
+
+    @GetMapping(path ="/liststudents")
     public String studentsTable(Model model){
         List<Student> students = studentService.findAll();
         model.addAttribute("students", students);
         return "studentstable";
     }
-
 }
